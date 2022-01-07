ce3ec633b3a54bb68ee063e47356bb979366b976
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-01-07 15:00:38 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import AppBar from './components/AppBar/AppBar';
 import Note from './components/Note/Note';
-import Background from './components/Background/Background';
 
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <div style={{ background: `url('${process.env.PUBLIC_URL}/images/food_img.jpg')`, height: '400px', width: '100%' }}>
       <AppBar />
-
-      <div >
-        <Background > <Note /></Background>
-
-        {/* <div style={{'backgroundColor':'red','height':'2000px'}}></div> */}
-      </div>
     </div>
+
   );
 }
 
